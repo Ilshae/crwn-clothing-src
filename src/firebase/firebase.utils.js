0e83282258704b2feb6da69bb1fa88c041021312
Ingrid Pruszynska <pruszynskaingrid@outlook.com>
2020-12-02 12:57:40 +0100

    Created user and cart sagas for sign in, sign up sign in with google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,9 +20,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   const userRef = firestore.doc(`users/${userAuth.uid}`)
 
-  const collectionRef = firestore.collection('users')
-  const collectionSnapshot = await collectionRef.get()
-
   const snapShot = await userRef.get()
 
   if (!snapShot.exists) {
@@ -70,11 +67,20 @@ export const convertCollectionsSnapshotToMap = collections => {
   }, {})
 }
 
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe()
+      resolve(userAuth)
+    }, reject)
+  })
+}
+
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({ prompt: 'select_account' })
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+googleProvider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
 export default firebase
